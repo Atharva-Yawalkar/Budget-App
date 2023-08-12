eb3a15405347c1ad230bb0dc9acf2d0201bb4bd2
Atharva Yawalkar <yawalkaratharva4@gmail.com>
2023-08-12 18:00:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import UncategorizedBudgetCard from "./components/UncategorizedBudgetCard"
 import TotalBudgetCard from "./components/TotalBudgetCard"
 import { useState } from "react"
 import { UNCATEGORIZED_BUDGET_ID, useBudgets } from "./contexts/BudgetsContext"
+import './App.css';
 
 function App() {
   const [showAddBudgetModal, setShowAddBudgetModal] = useState(false)
